213e0277d9a40834a56729c276341bc1b2ea727c
Mausham Neupane <maushamneupane2060@gmail.com>
2024-06-09 19:38:08 +0545

    cart page and order page created

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,16 @@
-import React, { useState } from 'react'; // Import useState from React
+import React, { useContext, useState } from 'react'; // Import useState from React
 import './Navbar.css';
 import { assets } from '../../assets/assets';
+import { Link } from 'react-router-dom';
+import { StoreContext } from '../../context/StoreContext';
 
 const Navbar = () => {
   const [menu, setMenu] = useState("home");
+  const {getTotalCartAmount}= useContext(StoreContext);
 
   return (
     <div className='navbar'>
-      <img src={assets.logo} alt="" className="logo" />
+      <Link to='/'><img src={assets.logo} alt="" className="logo" /></Link>
       <ul className="navbar-menu">
         <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</li>
         <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</li>
@@ -17,8 +20,9 @@ const Navbar = () => {
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
-          <img src={assets.basket_icon} alt="" />
-          <div className="dot"></div>
+          <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
+          
+          <div className={getTotalCartAmount()===0?"":"dot"}></div>
         </div>
         <button>Sign In</button>
       </div>
--- frontend/src/Pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/Pages/PlaceOrder/PlaceOrder.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
-import './PlaceOrder.css'
+import React, { useContext } from "react";
+import "./PlaceOrder.css";
+import { StoreContext } from "../../context/StoreContext";
 const PlaceOrder = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const {getTotalCartAmount}=useContext(StoreContext)
 
-export default PlaceOrder
+  return (
+    <form className="place-order">
+      <div className="place-order-left">
+        <p className="title">Order Information</p>
+        <div className="multi-fields">
+          <input type="text" placeholder="First Name" />
+          <input type="text" placeholder="Last Name" />
+        </div>
+        <input type="email" placeholder="Email Address" />
+        <input type="number" placeholder="Phone Number" />
+      </div>
+      <div className="place-order-right">
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>Rs. {getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>Rs. {0}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>Rs. {getTotalCartAmount()}</b>
+            </div>
+          </div>
+          <button>
+            PROCEED TO PAYMENT
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default PlaceOrder;
